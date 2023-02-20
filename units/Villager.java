e51c6fb8b1e82e6dfd6359effeff2a19beb1b32f
SeregadD90 <seregad07@mail.ru>
2023-02-21 03:18:29 +0700

    Персонажи

---
+++ units/Villager.java
@@ -0,0 +1,17 @@
+package units;
+
+/**Крестьянин*/
+public class Villager extends Warrior {
+  
+
+    public Villager() {
+        super("Villager__", "Green", 1, 3, 1, 0, 1, 1, 1);
+       
+    }
+
+
+    /**Переопределить getInfo так, чтобы он возвращал строки:"Я крестьянин", "Я снайпер"  */
+    @Override
+    public String getInfo() {return "Я Крестьянин ";}
+
+}
